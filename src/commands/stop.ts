ec60a52869124b744eef8c11583191c5af34c871
Shockch4rge <favouriteteo@gmail.com>
2021-11-20 22:09:06 +0800

    Added more commands

---
+++ src/commands/stop.ts
@@ -0,0 +1,40 @@
+import { InteractionFile } from "../helpers/BotHelper";
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { GuildMember, MessageEmbed } from "discord.js";
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("stop")
+        .setDescription("Clears the queue and stops the player."),
+
+    execute: async helper => {
+        const member = helper.interaction.member as GuildMember;
+        if (!helper.isMemberInBotVc(member)) {
+            return await helper.respond(new MessageEmbed()
+                .setAuthor("❌  We must be in the same voice channel to use this command!")
+                .setColor("RED"));
+        }
+
+        const service = helper.cache.service;
+
+        if (!service) {
+            return await helper.respond(new MessageEmbed()
+                .setAuthor("❌  I am not currently in a voice channel!")
+                .setColor("RED"));
+        }
+
+        try {
+            await service.stop();
+        }
+        catch (e) {
+            return await helper.respond(new MessageEmbed()
+                .setAuthor(`❌  ${e}`)
+                .setColor("RED"));
+        }
+
+        await helper.respond(new MessageEmbed()
+            .setAuthor("✅  Cleared queue and stopped the player.")
+            .setColor("GREEN"));
+    }
+
+} as InteractionFile;
---
+++ src/commands/test-audio.ts
@@ -0,0 +1,47 @@
+// This command is for testing audio connections.
+// Plays a 10-second audio clip from Youtube.
+import { InteractionFile } from "../helpers/BotHelper";
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { GuildMember, MessageEmbed } from "discord.js";
+import Song from "../models/Song";
+import { DiscordGatewayAdapterCreator, joinVoiceChannel } from "@discordjs/voice";
+import MusicService from "../services/MusicService";
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("test-audio")
+        .setDescription("Play a 10-second clip from Youtube to test the audio connection."),
+
+    execute: async helper => {
+        const member = helper.interaction.member as GuildMember;
+
+        if (!helper.cache.service) {
+            const connection = joinVoiceChannel({
+                guildId: member.guild.id,
+                channelId: member.voice.channelId!,
+                adapterCreator: member.guild.voiceAdapterCreator as DiscordGatewayAdapterCreator,
+            });
+
+            helper.cache.service = new MusicService(connection);
+        }
+
+        const service = helper.cache.service;
+        const url = "https://www.youtube.com/watch?v=wN9bXy_fiOE"
+
+        const song = await Song.from(url, helper.cache.apiHelper, member.displayName);
+
+        try {
+            await service.play(song);
+        }
+        catch (e) {
+            return await helper.respond(new MessageEmbed()
+                .setAuthor(`❌  ${e}`)
+                .setColor("RED"));
+        }
+
+        await helper.respond(new MessageEmbed()
+            .setAuthor("✅  Playing...")
+            .setColor("GREEN"));
+    }
+
+} as InteractionFile;
