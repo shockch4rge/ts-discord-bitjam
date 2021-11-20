6fae029aead1a65ebe602648afcd19cb9d52de83
Shockch4rge <favouriteteo@gmail.com>
2021-11-20 11:54:37 +0800

    Emit custom reject errors

--- src/services/MusicService.ts
+++ src/services/MusicService.ts
@@ -2,6 +2,7 @@ import {
     AudioPlayer,
     AudioPlayerStatus,
     createAudioPlayer,
+    entersState,
     VoiceConnection,
     VoiceConnectionStatus
 } from "@discordjs/voice";
@@ -55,12 +56,23 @@ export default class MusicService {
     }
 
     private setupConnectionListeners() {
-        this.connection.on(VoiceConnectionStatus.Ready, async (oldState, newState) => {
-        });
-
-        this.connection.on(VoiceConnectionStatus.Destroyed, async (oldState, newState) => {
+        this.connection.on(VoiceConnectionStatus.Ready, async oldState => {
 
         });
+
+        this.connection.on(VoiceConnectionStatus.Destroyed, async oldState => {
+
+        });
+
+        this.connection.on(VoiceConnectionStatus.Disconnected, async oldState => {
+            try {
+                this.connection.rejoin();
+                await entersState(this.connection, VoiceConnectionStatus.Ready, 10000);
+            }
+            catch {
+
+            }
+        })
     }
 
     public play(song: Song): Promise<void> {
@@ -68,7 +80,11 @@ export default class MusicService {
             this.prepend(song);
             this.queue[0]
                 .createAudioResource()
-                .then(resource => this.player.play(resource));
+                .then(resource => this.player.play(resource))
+                .catch(() => {
+                    reject("Failed to create resource.");
+                });
+
             resolve();
         });
     }
@@ -76,7 +92,7 @@ export default class MusicService {
     public pause(): Promise<void> {
         return new Promise((resolve, reject) => {
             if (this.player.state.status !== AudioPlayerStatus.Paused) {
-                reject();
+                reject("The bot is still paused!");
             }
 
             this.player.pause();
@@ -87,7 +103,7 @@ export default class MusicService {
     public resume(): Promise<void> {
         return new Promise((resolve, reject) => {
             if (this.player.state.status !== AudioPlayerStatus.Paused) {
-                reject();
+                reject("The bot is still playing!");
             }
 
             this.player.unpause();
@@ -98,7 +114,7 @@ export default class MusicService {
     public shuffle(): Promise<void> {
         return new Promise((resolve, reject) => {
             if (this.queue.length <= 0) {
-                reject();
+                reject("There are no songs in the queue.");
             }
 
             ArrayUtils.shuffle(this.queue);
@@ -130,21 +146,27 @@ export default class MusicService {
         this.queue.push(song);
     }
 
-    public move(fromIndex: number, toIndex: number): Promise<void> {
+    public moveSong(atIndex: number, toIndex: number): Promise<void> {
         return new Promise((resolve, reject) => {
-            if (fromIndex < 0 || toIndex >= this.queue.length) {
-                reject();
+            if (atIndex < 0 || toIndex >= this.queue.length) {
+                reject(`Invalid index! Provided (${atIndex}) and (${toIndex})`);
             }
 
-            ArrayUtils.move(fromIndex, toIndex, this.queue);
+            ArrayUtils.move(atIndex, toIndex, this.queue);
             resolve();
         });
     }
 
-    public remove(song: Song): Promise<void> {
+    public remove(index: number): Promise<void> {
         return new Promise((resolve, reject) => {
-            if (this.queue.indexOf(song) === -1) {
-                reject();
+            if (index === this.queue.length || index < 0) {
+                reject(`Invalid index! Provided (${index}).`);
+            }
+
+            const song = this.queue.at(index);
+
+            if (!song) {
+                reject(`Song not found at index (${index}).`);
             }
 
             ArrayUtils.remove(song, this.queue);
@@ -152,10 +174,32 @@ export default class MusicService {
         });
     }
 
+    public skip(): Promise<void> {
+        return new Promise((resolve, reject) => {
+            // get the next song first. If it doesn't exist, we've reached the end of the queue
+            const song = this.queue.at(1);
+
+            this.queue.shift();
+
+            if (!song) {
+                this.player.stop();
+                return;
+            }
+
+            try {
+                this.play(song)
+                    .then(resolve);
+            }
+            catch (e) {
+                reject(e);
+            }
+        });
+    }
+
     public swap(indexOne: number, indexTwo: number) {
         return new Promise((resolve, reject) => {
             if (indexOne >= this.queue.length || indexTwo >= this.queue.length) {
-                reject();
+                reject(`Invalid indexes! Provided (${indexOne}), (${indexTwo}).`);
             }
 
             ArrayUtils.swap(this.queue[indexOne], this.queue[indexTwo], this.queue);
