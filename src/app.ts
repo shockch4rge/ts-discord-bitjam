6ef4f9df556cb8340c3622d4c718acfa9fe5a822
Shockch4rge <favouriteteo@gmail.com>
2021-11-24 21:05:46 +0800

    Host local server to authenticate Spotify credentials

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,84 @@
 import { Client, Intents } from "discord.js";
+import express from "express";
+import open from "open";
 import BotHelper from "./helpers/BotHelper";
+import axios from "axios";
+import qs from "qs";
+import fs from "fs/promises";
+import path from "path";
 
 const auth = require("../auth.json")
 
 
-const bot = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES,]});
+const start = () => {
+    const PORT = 4296;
+    const app = express();
 
-const botHelper = new BotHelper(bot)
-botHelper.setup();
+    app.get("/", async (req, res) => {
+        console.log(`Got Spotify API Authorization token`)
+        const code = req.query.code as string;
 
-void bot.login(auth.bot_token);
+        // Login to Spotify and receive access and refresh token
+        const spotify_res = await axios.post(
+            "https://accounts.spotify.com/api/token",
+            qs.stringify({
+                grant_type: "authorization_code",
+                code,
+                redirect_uri: "http://localhost:4296"
+            }),
+            {
+                headers: {
+                    Authorization:
+                        "Basic " +
+                        Buffer.from(
+                            auth.spotify.clientId + ":" + auth.spotify.clientSecret
+                        ).toString("base64"),
+                    "Content-Type": "application/x-www-form-urlencoded"
+                }
+            }
+        );
+
+        // Replace old access token with new access token
+        const config_path = path.join(__dirname, "../auth.json")
+        const config_data = await fs.readFile(config_path, "utf8")
+        await fs.writeFile(
+            config_path,
+            config_data
+                .replace(auth.spotify.accessToken, spotify_res.data.access_token)
+                .replace(auth.spotify.refreshToken, spotify_res.data.refresh_token)
+        )
+        console.log(`Replaced Spotify API Access token`)
+
+        res.send("<script>window.close();</script>")
+        server.close()
+
+        const startBot = () => {
+            const bot = new Client({
+                intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES]
+            });
+
+            const botHelper = new BotHelper(bot)
+            botHelper.setup();
+
+            void bot.login(auth.bot_token);
+        }
+
+        startBot();
+    });
+
+    const server = app.listen(PORT, async () => {
+        console.log(`Server running on port ${PORT}`);
+
+        // Get spotify authorization code
+        const spotify_url = new URL("https://accounts.spotify.com/authorize");
+        spotify_url.searchParams.append("response_type", "code");
+        spotify_url.searchParams.append("client_id", auth.spotify.clientId);
+        spotify_url.searchParams.append("redirect_uri", "http://localhost:4296");
+        spotify_url.searchParams.append("scope", "user-read-private playlist-read-private");
+        await open(spotify_url.href, { wait: true });
+    });
+}
+
+
+
+start();
