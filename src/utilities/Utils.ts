c3e5850de9c892678e6c7975b054649735ca55a7
Shockch4rge <favouriteteo@gmail.com>
2021-11-25 13:42:00 +0800

    Fixed duration formatting bug

--- src/utilities/Utils.ts
+++ src/utilities/Utils.ts
@@ -13,8 +13,8 @@ export function delay(ms: number): Promise<unknown> {
 }
 
 export function formatTime(ms: number): string {
-    const min = ms / 1000 / 60;
-    const sec = ms / 1000 % 60;
+    const min = Math.floor(ms / 1000 / 60);
+    const sec = Math.floor(ms / 1000 % 60);
 
     return `${min}:${(sec < 10) ? "0" + sec : sec}`
 }
