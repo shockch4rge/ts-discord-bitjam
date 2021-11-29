d3d69ebdfabc1c5a1cf180e63ecf4ad392809fd7
Shockch4rge <favouriteteo@gmail.com>
2021-11-29 22:22:50 +0800

    Misc changes

--- src/models/Track.ts
+++ src/models/Track.ts
@@ -1,5 +1,5 @@
 import { AudioResource, createAudioResource, demuxProbe } from "@discordjs/voice";
-import { raw as ytdl } from 'youtube-dl-exec';
+import { raw as ytdl } from "youtube-dl-exec";
 import { ApiHelper } from "../helpers/ApiHelper";
 
 export default class Track implements TrackData {
@@ -45,7 +45,7 @@ export default class Track implements TrackData {
 
                     case "www.youtube.com":
                         if (!url.searchParams.get("list") && url.searchParams.get("v")) {
-                            possibleTracks = apiHelper.getYoutubeTrack(url.searchParams.get("v")!, requester)
+                            possibleTracks = apiHelper.getYoutubeTrack(url.searchParams.get("v")!, requester);
                         }
                         else if (url.searchParams.get("list") && url.searchParams.get("v")) {
                             possibleTracks = apiHelper.getYoutubePlaylist(url.searchParams.get("list")!, requester);
@@ -75,16 +75,16 @@ export default class Track implements TrackData {
     /**
      * Creates an AudioResource from this Track.
      */
-    public createAudioResource(settings: keyof typeof AudioQuality): Promise<AudioResource<Track>> {
+    public createAudioResource(quality: AudioQuality): Promise<AudioResource<Track>> {
         return new Promise((resolve, reject) => {
-            const [frequency, bitrate, quality] = AudioQuality[settings];
+            const [frequency, bitrate, _quality] = AudioQualityPresets[quality];
 
             // i have no idea what this does
             const process = ytdl(
                 this.url,
                 {
                     o: '-',
-                    q: quality,
+                    q: _quality,
                     f: `bestaudio[ext=webm+acodec=opus+asr=${frequency}]/bestaudio`,
                     r: bitrate,
                 },
@@ -137,8 +137,10 @@ export interface TrackData {
     requester: string,
 }
 
-export const AudioQuality = {
+export type AudioQuality = keyof typeof AudioQualityPresets;
+
+export const AudioQualityPresets = {
     LOW: ["16000", "32K", "5"],
     MEDIUM: ["32000", "64K", "3"],
     HIGH: ["48000", "128K", "0"],
-} as const
+} as const;
